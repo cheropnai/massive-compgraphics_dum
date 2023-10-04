9fb97376ca962e2e99f6b540c47daef1361f64c8
Franklin <85135768+K-NURF@users.noreply.github.com>
2023-10-04 12:00:36 +0300

    updated script to read only jsonl files

--- src/generate1.sh
+++ src/generate1.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
-# datasetPath = "data/massive_dataset/data/"
+# !/bin/bash
 
 # Get a list of all languages in the dataset.
-languages=$(ls 'data/massive_dataset/data/')
+cd data/massive_dataset/data/ || exit
+
+languages=$(ls *.jsonl)
+# languages=$(ls *.jsonl | sed 's/.jsonl//g')
 # echo "Languages in the dataset: $languages"
 
 # Loop through the languages and run the main.py script for each language.
-cd src || exit
+cd ../../../src || exit
 for language in $languages; do
-  python src/main.py --language $language
+  python main.py --language $language
 done
